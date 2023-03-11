3408f4990f9453bd5a0ed325f6a573eb638a7e76
vidura-kaushalya <vidura.in@gmail.com>
2023-03-11 13:02:17 +0530

    created sign in page

--- src/Pages/Login/Auth/googleAuth.jsx
+++ src/Pages/Login/Auth/googleAuth.jsx
@@ -1,31 +1,49 @@
-import React from 'react'
-import {GoogleLogin} from 'react-google-login';
+import { Button, Grid } from "@mui/material";
+import React, { useEffect } from "react";
+import { GoogleLogin } from "react-google-login";
+import { makeStyles } from "@mui/styles";
+
+// Icons
+import { FcGoogle } from "react-icons/fc";
 
 // Google Login client Id
-const clientId = '512570607110-rnt0aersvlk9btgbhaqrv8rrehpq3udr.apps.googleusercontent.com';
+const clientId =
+  "512570607110-rnt0aersvlk9btgbhaqrv8rrehpq3udr.apps.googleusercontent.com";
 
-function googleAuth() {
+function GoogleAuth() {
 
-    const onSuccess = (res) => {
-        console.log('[Login Success] currentUser:', res.profileObj);
-    };
+  const onSuccess = (res) => {
+    console.log("[Login Success] currentUser:", res.profileObj);
+  };
 
-    const onFailure = (res) => {
-        console.log('[Login failed] res:', res);
-    };
+  const onFailure = (res) => {
+    console.log("[Login failed] res:", res);
+  };
 
   return (
-    <div className="google-btn">
-        <GoogleLogin
-            clientId={clientId}
-            buttonText="Login"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
-            cookiePolicy={'single_host_origin'}
-            isSignedIn={true}
-        />
-    </div>
-  )
+    <Grid >
+      <GoogleLogin
+        clientId={clientId}
+        render={(renderProps) => (
+          <Button
+            style={{border : "2px solid #bdbdbd", textTransform: "none",
+            display: "flex", alignItems: "center", justifyContent: "center",
+            width: "100%", height: "100%", borderRadius: "5px", gap: "10px",
+            color: "#808080", fontFamily: "fontFamily: 'Inter'", fontWeight: "500", borderRadius: "7px"}}     
+            onClick={renderProps.onClick}
+            disabled={renderProps.disabled}
+          >
+            <FcGoogle size={20}/>
+            Google
+          </Button>
+        )}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={"single_host_origin"}
+      />
+    </Grid>
+  );
 }
 
-export default googleAuth
+export default GoogleAuth;
--- src/Pages/Login/SignIn/SignIn.jsx
+++ src/Pages/Login/SignIn/SignIn.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { useEffect } from "react";
-import { Grid, InputAdornment, TextField } from "@mui/material";
+import { Button, Grid, InputAdornment, TextField } from "@mui/material";
 import "./SignIn.css";
+//MUI
+import Fab from '@mui/material/Fab';
 
 //GOOGLE LOGIN
-import LoginButton from "../Auth/googleAuth";
+import LoginButton from "../Auth/GoogleAuth";
+// import LogoutButton from "../Auth/Logout";
 import {gapi} from 'gapi-script';
 
 
 // Icons
 import { BsShieldLock } from "react-icons/bs";
 import { FiMail } from "react-icons/fi";
+import CloseIcon from "@mui/icons-material/Close";
 
 // check whether device is mobilr or not
 import {isMobile} from 'react-device-detect';
@@ -34,17 +38,33 @@ function SignIn() {
   return (
     
     <Grid container className={isMobile ? "container-mobile" : "container"}>
-    <Grid item xs={12} md={6} className="container-left">
+    <Grid item xs={12} md={6} className="container-left" style={isMobile ? {display: "none"} : {display: "block"}}>
+      <div className="close-btn">
+        <Fab size="small" aria-label="add">
+          <CloseIcon />
+        </Fab>
+      </div>
         <Grid className="signin-image">
           <img src={require('../../../assets/images/login.png')} alt="signin"/>
           </Grid>
       </Grid>
+          {/* <Grid className="powerdby">
+            <h5>Powered by</h5>
+            <img src={require('../../../assets/images/powerdBy.png')} alt="signin"/>
+            </Grid> */}
       <Grid item xs={12} md={6} className="container-right">
         <form>
           <div className="form-wrapper">
+            <div className="logo">
+              <img src={require('../../../assets/images/Logo.png')} alt="logo"/>
+            </div>
+            <div className="powerdby">
+            <h5>Powered by</h5>
+            <img src={require('../../../assets/images/powerdBy.png')} alt="signin"/>
+            </div>
             <div className="heading">
               <h1>Sign In to your Account</h1>
-              <h5>Welcome Back. Select method to Login</h5>
+              <h5>Welcome Back. Select a method to Login</h5>
             </div>
             <div className="social-login">
               <div className="google-btn">
@@ -55,9 +75,14 @@ function SignIn() {
 
               </div>
             </div>
+            <div className="or">
+            
+              <h5>or countinue with email</h5>
+             
+            </div>
             <TextField
               InputProps={{
-                style: { fontSize: 13, borderRadius: 6 },
+                style: { fontSize: 14, borderRadius: 6, fontFamily: 'Inter' },
                 startAdornment: (
                   <InputAdornment position="start">
                     <FiMail size={16} />
@@ -71,7 +96,7 @@ function SignIn() {
             ></TextField>
             <TextField
               InputProps={{
-                style: { fontSize: 13, borderRadius: 6 },
+                style: { fontSize: 14, borderRadius: 6, fontFamily: 'Inter' },
                 startAdornment: (
                   <InputAdornment position="start">
                     <BsShieldLock size={16} />
@@ -80,9 +105,16 @@ function SignIn() {
               }}
               id="outlined-size-small"
               placeholder="Password"
+              type="password"
               size="small"
               fullWidth
             />
+            <div className="signin-btn">
+            <Button variant="contained" className="signBtn">Sign In</Button>
+            </div>
+            <div className="signup-link">
+              <h5>Don't have an account? <a href="/signup">Create an account</a></h5>
+              </div>
           </div>
         </form>
       </Grid>
